725b92dd53344b227ed081d1e62ec430691a03d8
magdalena <magdalena.janeckova92@gmail.com>
2024-11-04 09:59:38 +0000

    CORS

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+const cors = require('cors');
 const express = require("express");
 const app = express();
 const apiRouter = require("./routes/api-router");
 
 const { psqlErrorHandlerOne, psqlErrorHandlerTwo, psqlErrorHandlerThree, customErrorHandler, serverErrorHandler } = require("./error-handlers");
 
+app.use(cors());
 app.use(express.json());
 app.use("/api", apiRouter);
 
